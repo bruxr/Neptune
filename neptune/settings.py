e2f497aa8b573cc67913fed8fdb503c9a40741f9
Brux Romuar <brux.romuar@gmail.com>
2019-02-03 03:28:32 +0800

    Use new secret key

--- neptune/settings.py
+++ neptune/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!k7ag$^18cg7gfzv8dip0)n)22ou*7azg5&e!$gx2(veftyecw'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
